f88c0dca98779cdc755f2e22ac958e6951ec0edb
Badru Bulafu Masole <elosambadr@gmail.com>
2023-02-16 01:57:48 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,6 @@
  * main -Program that prints a line using the printf function
  * Return: 0 (Successful)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
